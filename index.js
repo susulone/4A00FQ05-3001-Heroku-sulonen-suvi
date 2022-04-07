54ef4106feb3b7aaa98243e06da75c152bd7ecf6
Suvi.Sulonen <suvi.sulonen@tuni.fi>
2022-04-07 10:58:50 +0300

    Create: index.js, package.json, package-lock.json. Add: (index.js) Simple server connection.

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 8080;
+
+const db = [{ name: "tiina" }, { name: "jack" }];
+
+app.get("/names", (req, res) => {
+  res.send(db);
+});
+
+const server = app.listen(8080, () => {
+  console.log(`Listening on port ${port}`);
+});

83248d4916dd497426fe922d184b4e6e325d5bf1
Suvi.Sulonen <suvi.sulonen@tuni.fi>
2022-04-07 11:23:18 +0300

    Edit: (index.js) Create connection to the database.

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express = require("express");
+const mysql = require("mysql");
 const app = express();
 const port = process.env.PORT || 8080;
 
-const db = [{ name: "tiina" }, { name: "jack" }];
+let config = {
+  host: "mydb.tamk.fi",
+  user: process.env.user,
+  password: process.env.password,
+  database: process.env.database,
+};
 
-app.get("/names", (req, res) => {
-  res.send(db);
+const connection = mysql.createConnection(config);
+
+app.get("/", (req, res) => {
+  connection.query("SELECT * from location", (error, results) => {
+    if (error) {
+      console.log(error);
+    } else {
+      res.send(results);
+    }
+  });
 });
 
 const server = app.listen(port, () => {
